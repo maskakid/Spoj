469775d47a45525530c998d281a6da9f2e7b8533
maskakid <akidmask@gmail.com>
2015-09-29 18:16:52 +0600

    29.9.2015

---
+++ TRICOUNT-Counting Triangles.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#define LL unsigned long long int
+using namespace std;
+LL n,t;
+int main()
+{
+    scanf("%llu",&t);
+    while(t--)
+    {
+        scanf("%llu",&n);
+        printf("%llu\n",((n*(n+2)*(2*n+1))/8));
+    }
+    return 0;
+}
